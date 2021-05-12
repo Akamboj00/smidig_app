69ba92566c00f6804c2e91e80a32b2d0209da596
abhil <abhilash.kamboj@gmail.com>
2021-05-12 13:24:59 +0200

    restart

--- app/src/main/java/com/example/report_app/LoginActivity.kt
+++ app/src/main/java/com/example/report_app/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +14,65 @@ class LoginActivity : AppCompatActivity() {
         tv_register.setOnClickListener{
             startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
         }
+
+        btn_login.setOnClickListener {
+            when {
+                //Checks that the email input field is not empty and shows message
+                TextUtils.isEmpty(et_login_email.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "please enter email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                //Checks that the password input field is not empty and shows message
+                TextUtils.isEmpty(et_login_password.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "please enter password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    val email: String = et_login_email.text.toString().trim { it <= ' ' }
+                    val password: String = et_login_password.text.toString().trim { it <= ' ' }
+
+                    //Login with info given above
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                                //Show message if login is successful
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    "You were registered successfully",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+
+                                val intent = Intent(this@LoginActivity, MainActivity :: class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                intent.putExtra("user_id",
+                                    FirebaseAuth.getInstance().currentUser!!.uid)
+                                intent.putExtra("email_id", email)
+                                startActivity(intent)
+                                finish()
+                            }else{
+                                //If the login is not successful then show error message
+                                Toast.makeText(
+                                    this@LoginActivity,
+                                    task.exception!!.message.toString(),
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+
+                }
+            }
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/report_app/RegisterActivity.kt
+++ app/src/main/java/com/example/report_app/RegisterActivity.kt
@@ -55,7 +55,7 @@ class RegisterActivity : AppCompatActivity() {
                                     Toast.LENGTH_SHORT
                                 ).show()
 
-                                val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+                                val intent = Intent(this@RegisterActivity, MainActivity :: class.java)
                                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                                 intent.putExtra("user_id", firebaseUser.uid)
                                 intent.putExtra("email_id", email)
